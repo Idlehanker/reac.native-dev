12af360bde305fe752ccc17f4b1d253bc81b127b
idlehanker <hzujun@hotmail.com>
2018-03-24 00:28:28 +0800

    up

--- reactnavigation/src/pages/DetailsPage.js
+++ reactnavigation/src/pages/DetailsPage.js
@@ -3,10 +3,32 @@ import {StyleSheet, View, Text, Button} from 'react-native'
 
 export default class DetailsPage extends React.Component {
 
+    // static navigationOptions = {     title: 'Details' }
+    static navigationOptions = ({navigation}) => {
+        const {params} = navigation.state
+        return {
+            title: params
+                ? params.otherParam
+                : 'A Nested Details Screen'
+        }
+    }
+
     render() {
+        const {params} = this.props.navigation.state
+        const itemId = params
+            ? params.itemId
+            : null
+        const otherParam = params
+            ? params.otherParam
+            : null
+
         return (
             <View style={styles.container}>
+
                 <Text>This is the details page</Text>
+                <Text>itemId: {JSON.stringify(itemId)}</Text>
+                <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+
                 <Button
                     title="Go to Details"
                     onPress={() => {
--- reactnavigation/src/pages/HomePage.js
+++ reactnavigation/src/pages/HomePage.js
@@ -3,6 +3,11 @@ import {StyleSheet, View, Text, Button} from 'react-native'
 import {StackNavigator} from 'react-navigation'
 
 export default class HomePage extends React.Component {
+
+    static navigationOptions = {
+        title: 'Home'
+    }
+    
     render() {
         return (
             <View style={styles.container}>
@@ -10,7 +15,10 @@ export default class HomePage extends React.Component {
                     This is Home Page</Text>
                 <Button
                     title='Go to Details'
-                    onPress={() => this.props.navigation.navigate('Details')}/>
+                    onPress={() => this.props.navigation.navigate('Details', {
+                    itemId: 88,
+                    otherParam: 'Anything you want here.'
+                })}/>
 
             </View>
         )
