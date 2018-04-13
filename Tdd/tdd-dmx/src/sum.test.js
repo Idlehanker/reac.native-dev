223bcfeafa6824a36e5c9e20c0ae091d7e4557b6
huzj <huzj2@cmschina.com.cn>
2018-04-13 18:21:31 +0800

    d'f

--- Tdd/tdd-dmx/src/sum.test.js
+++ Tdd/tdd-dmx/src/sum.test.js
@@ -8,3 +8,9 @@ test('adds 1 + 2 to equal 3', ()=>{
 test('two plus two is foure', ()=>{
   expect(2 + 2).toBe(4)
 })
+
+test('object assignment', ()=>{
+  const data = {one: 1}
+  data['two'] = 2
+  expect(data).toEqual({one: 1, two: 2})
+})
