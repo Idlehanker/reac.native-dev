e572c6aa29e5294916d8ee5ad0b44b12c255bf96
huzj <huzj2@cmschina.com.cn>
2018-05-28 16:23:31 +0800

    merge

--- quickstart/Component/index.js
+++ quickstart/Component/index.js
@@ -6,6 +6,6 @@ import {Platform} from 'react-native';
 // __STRESS_TEST__ = false;
 import App from './bootstrap';
 // AppRegistry.registerComponent('Component', () => App); const app = new App();
-// if (Platform.OS === 'ios'){
-const app = new App();
-// }
+if (Platform.OS == 'ios') {
+    const app = new App({});
+}
